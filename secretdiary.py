bdd9ff51b4cf1f626b6eb325b9e748e6489f2785
Parthseh123 <86557891+Parthseh123@users.noreply.github.com>
2022-02-28 15:02:56 +0530

    Update secretdiary.py

--- secretdiary.py
+++ secretdiary.py
@@ -1,6 +1,15 @@
+from operator import add
+
+
 Username = "Parth"
 diarynickname = "DiBuddy"
-password = "p"
+password = input("Enter password you want to set - ")
+passwordfw = open(password,"w")
+passwordfw.write(password)
+# with open(password.txt, 'r') as f:
+#     a = f.read()
+passwordfw = open(password)
+pinside= passwordfw.read()
 n=0
 
 diary_code = {}
@@ -9,32 +18,34 @@ def addorsee():
     add_or_see = input("Do you want to add or see - ")
     while(n < 10):
         if("add" in add_or_see):
-            addpgnum = int(input("Enter you Page number - "))
+            addtitle = input("Enter you title - ")
             add1page=input("Enter your text - ")
+            diary_code.update({addtitle : add1page})
+            # pagefile= open(addtitle,"w")
+            # pagefile.write(add1page)
             
-            diary_code.update({addpgnum : add1page})
             add_or_see = input("Do you want to add or see - ")
 
         if("see" in add_or_see):
-            pgnumber=int(input("Enter the page number you want to see - "))
-            print(diary_code[pgnumber])
+            pgnumber=input("Enter the page number you want to see - ")
+            print(diary_code[addtitle])
             add_or_see = input("Do you want to add or see - ")
 
 
                 
 
 open = input("Enter The password to open the diary - ")
-if(open == password):
-        addorsee()    
+if(open == pinside):
+    addorsee()    
 else:
     print("try again")
     open = input("Enter The password to open the diary - ")
-    if(open == password):
+    if(open in pinside):
         addorsee()    
     else:
         print("try again")
         open = input("Enter The password to open the diary - ")
-        if(open == password):
+        if(open in pinside):
             addorsee()
         else:
             print("Diary Locked")
